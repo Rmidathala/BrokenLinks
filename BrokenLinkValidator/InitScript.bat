cba7a4c72b1523b9b63234ba718d520143ded56e
Midathala <220097@cognizant.com>
2019-09-26 11:55:39 +0530

    Initial Commit

---
+++ BrokenLinkValidator/InitScript.bat
@@ -0,0 +1,3 @@
+D:
+cd "D:\WCB Workspace\BrokenLinksValidator"
+java -cp ".;.\supportlibraries\External_Jars\*" allocator.Allocator
---
+++ BrokenLinkValidator/componentgroups/CheckoutGroups.java
@@ -0,0 +1,19 @@
+package componentgroups;
+
+import supportlibraries.*;
+
+public class CheckoutGroups extends ReusableLibrary{
+	
+	
+	/**
+	 * Constructor to initialize the component group library
+	 * @param scriptHelper The {@link ScriptHelper} object passed from the {@link DriverScript}
+	 */
+
+	public CheckoutGroups(ScriptHelper scriptHelper) {
+		super(scriptHelper);
+	
+	}
+
+
+}
